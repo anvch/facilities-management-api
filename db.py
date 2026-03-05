1c457c1d5fb5f96d7759ce59099d497c6a60be68
William Liu <wliu40@calpoly.edu>
2026-03-04 16:47:15 -0800

    Updated db.py to override system variables with .env file

--- db.py
+++ db.py
@@ -2,8 +2,9 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
+# .env file overrides system environment variables
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("HOST"),

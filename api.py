fd842a76d441afe11214c6040e86363d174dad4e
William Liu <wliu40@calpoly.edu>
2026-03-04 16:46:28 -0800

    Turned permission level into enum. Updated validate_permission to take in affiliations.

--- api.py
+++ api.py
@@ -1,44 +1,91 @@
+from unittest import case
+
+from lxml.objectify import NoneElement
+
 from db import get_connection
 
+from enum import Enum
+
+class PermissionLevel(Enum):
+    GOD = "God Level"
+    DEPARTMENT_VIEW = "Department View Level"
+    DEPARTMENT_UPDATE = "Department Update Level"
+    COLLEGE_VIEW = "College View"
+    COLLEGE_UPDATE = "College Update Level"
+
+def convert_perm_level_to_enum(perm_level):
+    match perm_level:
+        case "God Level":
+            return PermissionLevel.GOD
+        case "Department View Level":
+            return PermissionLevel.DEPARTMENT_VIEW
+        case "Department Update Level":
+            return PermissionLevel.DEPARTMENT_UPDATE
+        case "College View":
+            return PermissionLevel.COLLEGE_VIEW
+        case "College Update Level":
+            return PermissionLevel.COLLEGE_UPDATE
+        case _:
+            raise Exception(f"Unknown permission level: {perm_level}")
+
 # 0. Check Permission (validatePermission)
-def validate_permission(user_id, perm_level):
-    permission_levels = {
-        "God Level": 1,
-        "College Update Level": 2,
-        "Department Update Level": 3,
-        "College View Level": 4,
-        "Department View Level": 5
-    }
+
+def validate_permission(user_id, required_perm_level, required_affiliations=None):
+    if required_perm_level not in PermissionLevel:
+        raise Exception("Invalid permission level")
 
     # connect to DB
-    conn = get_connection()
-    cursor = conn.cursor()
+    with get_connection() as conn:
+        with conn.cursor() as cursor:
+            # retrieve user permission and affiliations from DB
+            cursor.execute(
+                """
+                SELECT A.permission_level,A.college_code,D.id
+                FROM Accounts as A
+                WHERE username = %s;
+                """,(user_id,)
+            )
+            rows = cursor.fetchall()
+            user_perm_level = convert_perm_level_to_enum(rows[0][0])
 
-    # retrieve user permission from DB
-    cursor.execute(
-        "SELECT permission_level FROM Accounts WHERE username = %s",
-        (user_id,)
-    )
-    row = cursor.fetchone()
+            if user_perm_level == PermissionLevel.GOD:
+                return True
 
-    cursor.close()
-    conn.close()
+            if len(rows) == 0 or not perm_level_greater(user_perm_level, required_perm_level):
+                return False
 
-    if not row:
-        return False
+            if required_affiliations is not None:
+                if user_perm_level in [PermissionLevel.COLLEGE_VIEW, PermissionLevel.COLLEGE_UPDATE]:
+                    if required_affiliations['college'] == rows[0][1]:
+                        return True
+                    else:
+                        return False
+                if user_perm_level in [PermissionLevel.DEPARTMENT_VIEW, PermissionLevel.DEPARTMENT_UPDATE]:
+                    if rows[0][2] in required_affiliations['department']:
+                        return True
+                    else:
+                        return False
+                raise Exception("Unhandled Permission Level")
 
-    user_perm_level = row[0]
+            return True
 
-    # check permission level
-    if permission_levels[user_perm_level] <= permission_levels[perm_level]:
+def perm_level_greater(user_perm_level, required_perm_level):
+    # Array represents permission levels under the key in permission levels hierarchy
+    permission_levels = {
+        PermissionLevel.GOD: [PermissionLevel.GOD,PermissionLevel.DEPARTMENT_VIEW,PermissionLevel.DEPARTMENT_UPDATE,PermissionLevel.COLLEGE_VIEW,PermissionLevel.COLLEGE_UPDATE],
+        PermissionLevel.COLLEGE_UPDATE: [PermissionLevel.COLLEGE_UPDATE,PermissionLevel.COLLEGE_VIEW,PermissionLevel.DEPARTMENT_VIEW,PermissionLevel.DEPARTMENT_UPDATE],
+        PermissionLevel.DEPARTMENT_UPDATE: [PermissionLevel.DEPARTMENT_UPDATE,PermissionLevel.DEPARTMENT_VIEW],
+        PermissionLevel.COLLEGE_VIEW: [PermissionLevel.COLLEGE_VIEW,PermissionLevel.DEPARTMENT_VIEW],
+        PermissionLevel.DEPARTMENT_VIEW: [PermissionLevel.DEPARTMENT_VIEW]
+    }
+    if required_perm_level in permission_levels[user_perm_level]:
         return True
     else:
         return False
 
-
 # test validate permissions
-print(validate_permission("1", "Department View Level")) # expected True
-print(validate_permission("1", "God Level")) # expected False
+print(validate_permission("1", PermissionLevel.DEPARTMENT_VIEW)) # expected True
+print(validate_permission("1", PermissionLevel.GOD)) # expected False
 
 # -----------------------------------------------
 # DATA RETRIEVAL
@@ -46,7 +93,7 @@ print(validate_permission("1", "God Level")) # expected False
 # 4. Retrieve Room Information (getRoomInfo)
 def get_room_info(user_id, building_id, room_num):
     # validate permission level
-    if not validate_permission(user_id, "Department View Level"):
+    if not validate_permission(user_id, PermissionLevel.DEPARTMENT_VIEW):
         return None
     
     conn = get_connection()

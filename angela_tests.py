fcdff91c529ae2312ada3b3c6c68cca067637aff
Angela Chen <achen313@calpoly.edu>
2026-03-10 09:46:41 -0700

    delete william tests

--- angela_tests.py
+++ angela_tests.py
@@ -25,25 +25,6 @@ sys.stdout = open("achen_test_output.txt","a")
 # Data Retrieval ------------------------------------------------------
 print(header_string("DATA RETRIEVAL"))
 
-# 1. getFloorPlans()
-print("1. getFloorPlans()")
-print(label_result_format("Valid permissions (having an account):",get_floor_plans('wliu')))
-print(label_result_format("Invalid permission:",get_floor_plans('notReal')))
-
-# 2. getRooms()
-print("2. getRooms()")
-print(label_result_format("Valid permissions (having an account):",get_rooms('wliu','033-0',0)))
-print(label_result_format("Invalid permission:",get_rooms('notReal','033-0',0)))
-
-# 3. findRooms()
-print("3. getRooms()")
-# Just out of '053-0 0210-A0' boundary
-# * All of 053-0 floor 2 is wrapped within 0220-00. Will return 053-0 floor 2 if not in any others
-print(label_result_format("Just out of '053-0 0210-A0' boundary. Should return '053-0 0210-00':",find_room('wliu','053-0',2,(268,471))))
-
-# Just within '053-0 0210-A0' boundary
-print(label_result_format("Just within '053-0 0210-A0' boundary. Should return '053-0 0210-A0':",find_room('wliu','053-0',2,(268,473))))
-
 # 4. getRoomInfo()
 print("4. getRoomInfo()")
 print(label_result_format("Valid building id, Valid room num (053-0 0210-A0)", get_room_info("achen", "053-0", "0210-A0")))
@@ -63,27 +44,6 @@ print(label_result_format("Invalid college, Valid department name (ABCD, Mathema
 
 # Data Manipulation ------------------------------------------------------
 print(header_string("DATA MANIPULATION"))
-
-# 1. addEmployee()
-print("1. addEmployee() -- God Level permissions required")
-# Invalid permission to add employee
-print(label_result_format("Invalid permission (Department Update)",add_employee('wliu','John','Smith','jsmith@calpoly.edu','lecturer')))
-print(label_result_format("Invalid permission (College Update)",add_employee('rrodriguez','John','Smith','jsmith@calpoly.edu','lecturer')))
-# Valid permission
-print(label_result_format("Valid permission (God Level)",add_employee('achen','John','Smith','jsmith@calpoly.edu','lecturer')))
-# Try to add again
-print(label_result_format("Valid permission but employee already exists",add_employee('achen','John','Smith','jsmith@calpoly.edu','lecturer')))
-
-# 2. assignRoom()
-print("2. assignRoom() -- Department Affiliations required")
-# Invalid Permissions (Wrong college -- College update)
-print(label_result_format("Invalid Permissions (Wrong college -- College update)",assign_room('jadoe',471,'053-0','0220-00')))
-# Invalid Permissions (Wrong department -- Department update)
-print(label_result_format("Invalid Permissions (Wrong department -- Department update)",assign_room('gsmith',471,'053-0','0220-00')))
-# Valid permissions (College update)
-print(label_result_format("Valid permissions (College update)",assign_room('rrodriguez',471,'053-0','0220-00')))
-# Valid permissions (Department update)
-print(label_result_format("Valid permissions (Department update)",assign_room('wliu',471,'053-0','0220-00')))
 
 # 3. removeRoomAssignment()
 print("3. removeRoomAssignment() -- Department Affiliations required")
@@ -129,17 +89,3 @@ sleep(1)
 print(label_result_format("Assigning 033-0-0151-00 back to 15100",department_assignment('rrodriguez',115100,'033-0','0151-00')))
 # NOTE: need to use god-level perms here because we changed the dept
 print(label_result_format("Assigning 033-0-0001-00 back to 15100",department_assignment('achen',115100,'033-0','0001-00')))
-
-# Logging ------------------------------------------------------
-print(header_string("LOGGING"))
-
-# 1. logLogin()
-print("1. logLogin()")
-print(label_result_format("Valid permissions (having an account):",log_login('wliu@gmail.com')))
-print(label_result_format("Valid permissions (having an account):",log_login('notReal@gmail.com')))
-
-# 2. logLogout()
-print(2, "logLogout()")
-print(label_result_format("Valid permissions (having an account):",log_logout('wliu@gmail.com')))
-print(label_result_format("Valid permissions (having an account):",log_logout('notReal@gmail.com')))
-
